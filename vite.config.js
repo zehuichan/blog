3bee2a9f77b837e0dfc801d457262d30f148f659
Zehui Chan <jasonchenzehui@gmail.com>
2024-01-13 21:59:53 +0800

    test: cicd4

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import unocss from 'unocss/vite'
+import { defineConfig, loadEnv } from 'vite'
+import { wrapperEnv } from './build/utils'
+import { createVitePlugins } from './build/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
+  const root = process.cwd()
+
+  const env = loadEnv(mode, root)
+
+  const viteEnv = wrapperEnv(env)
+
+  const { VITE_PUBLIC_PATH } = viteEnv
+
+  const isBuild = command === 'build'
+
   return {
-    plugins: [
-      vue(),
-      unocss()
-    ],
+    base: VITE_PUBLIC_PATH,
+    root: root,
+    plugins: createVitePlugins(viteEnv, isBuild),
   }
 })
